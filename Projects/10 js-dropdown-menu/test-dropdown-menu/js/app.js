bbc18d4f732e5cfddfa75e555b115d1591e64452
mkaifnadeem <mkaifnadeem024@gmail.com>
2025-02-22 16:10:51 +0500

    ADD Project Dec to Work section

--- Projects/10 js-dropdown-menu/test-dropdown-menu/js/app.js
+++
@@ -1,8 +0,0 @@
-const dropdownList = document.querySelector(".nav-link-container");
-const dropdownButton = document.querySelector("#dropdown-button");
-
-function toggle() {
-  dropdownList.classList.toggle("hidden");
-}
-
-dropdownButton.addEventListener("click", toggle);
